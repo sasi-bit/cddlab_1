e113ccebf74f152146e36e7eb5ea520b274525cc
sasi-bit <55438592+sasi-bit@users.noreply.github.com>
2021-09-03 21:38:09 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will add two numbers
+    */
+
+   public static void main(String []args) {
+      int a = 3, b = 4;
+      Int sum = a + b;
+      System.out.println(sum); // prints 7
+   }
+}
